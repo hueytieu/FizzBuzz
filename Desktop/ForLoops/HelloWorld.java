7d30e36c9440ccefcd50aa73e1ee6bd2e35a3356
Huey Tieu <huey.tieu@mrjaffesclass.com>
2022-09-30 11:43:43 -0700

    fortheloops

---
+++ Desktop/ForLoops/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        for (int i = 0; i >= 15; i = i + 1) {
+            System.out.println("i is equal to 15");
+        }
+        for 
+    }
+
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
+
