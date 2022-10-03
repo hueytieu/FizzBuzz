7f4fb0e766333de54e380e2d9839e6ba617de63a
Huey Tieu <huey.tieu@mrjaffesclass.com>
2022-10-03 11:48:54 -0700

    Pooppants

--- Desktop/ForLoops/HelloWorld.java
+++ Desktop/ForLoops/HelloWorld.java
@@ -7,10 +7,17 @@ public class HelloWorld
     int d = -8;
     
     public void run() {
-        for (int i = 0; i >= 15; i = i + 1) {
-            System.out.println("i is equal to 15");
+        for (int i = 0; i <= 15; i = i + 1) {
+            System.out.println("i is equal to " + i);
         }
-        for 
+        for (int j = 10; j == 0; j = j - 2) {
+            System.out.println("j is equal to " + j);
+        }
+        for (int k = 10; k == 0; k = k - 3) {
+            System.out.println("k is equal to " + k);
+            //if you count down by 3 you wont reach 0 ever
+        }
+        
     }
 
     public static void main(String[] args)
