0c2468405ade5aaa3db5011c8aa89b49e64a53c9
Huey Tieu <huey.tieu@mrjaffesclass.com>
2022-10-19 11:16:01 -0700

    s

--- Desktop/ForLoops/HelloWorld.java
+++ Desktop/ForLoops/HelloWorld.java
@@ -1,23 +1,9 @@
 public class HelloWorld
 {
-
-    int a = 5;
-    int b = 2;
-    int c = 1;
-    int d = -8;
-    
     public void run() {
-        for (int i = 0; i <= 15; i = i + 1) {
-            System.out.println("i is equal to " + i);
+        for (int a = 1; a == 100; a = a + 1) {
+            System.out.println(a);
         }
-        for (int j = 10; j >= 0; j = j - 2) {
-            System.out.println("j is equal to " + j);
-        }
-        for (int k = 10; k >= 0; k = k - 3) {
-            System.out.println("k is equal to " + k);
-            //if you count down by 3 you wont reach 0 ever
-        }
-        
     }
 
     public static void main(String[] args)
